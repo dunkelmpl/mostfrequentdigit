bad56eb0ecb92679883342ecb6abdccf6b11ef4a
Pavel Martynau <dunkel@tut.by>
2017-11-24 00:43:49 -0800

    Minor code clearing.

--- MostFrequentDigit/Source.cpp
+++ MostFrequentDigit/Source.cpp
@@ -11,12 +11,12 @@ typedef unsigned long long ulonglong;
 // in a single number. The position of a digit in a map (0...n) relates to
 // the digit from the input number.
 // For example map = A2090 means:
-//		1. 0 of digit 0
-//		2. 9 of digit 1
-//		3. 0 of digit 2
-//		4. 2 of digit 3
-//		5. 10 of digit 4
-// 20-based map is used for storing counter of up to 19 for every digit,
+//        1. 0 of digit 0
+//        2. 9 of digit 1
+//        3. 0 of digit 2
+//        4. 2 of digit 3
+//        5. 10 of digit 4
+// 20-base map is used for storing counter of up to 19 for every digit,
 // since the biggest number with biggest quantity of same digits is
 // 9999999999999999999UL (19 of 9-digit)
 // After building the map we check every 20-base digit in the map and find
@@ -28,44 +28,43 @@ typedef unsigned long long ulonglong;
 //////////////////////////////////////////////////////////////////////////
 int getMostFrequentDigit(ulonglong input)
 {
-	const int mapBase = 20;
-	ulonglong twentyBasedDigitsMap = 0;
+    const int mapBase = 20;
+    ulonglong twentyBaseDigitsMap = 0;
 
-	while (input != 0) {
-		int digit = input % 10;
-		input = input / 10;
+    while (input != 0) {
+        int digit = input % 10;
+        input = input / 10;
 
-		twentyBasedDigitsMap += (ulonglong)pow(mapBase, digit);
-	}
+        twentyBaseDigitsMap += (ulonglong)pow(mapBase, digit);
+    }
 
-	int mostFrequentDigit = 0;
-	int maxFrequency = 0;
-	for (int digit = 0; twentyBasedDigitsMap != 0; digit++, twentyBasedDigitsMap /= mapBase) {
-		int frequency = twentyBasedDigitsMap % mapBase;
+    int mostFrequentDigit = 0;
+    int maxFrequency = 0;
+    for (int digit = 0; twentyBaseDigitsMap != 0; digit++, twentyBaseDigitsMap /= mapBase) {
+        int frequency = twentyBaseDigitsMap % mapBase;
 
-		if (frequency > 0 && frequency >= maxFrequency) {
-			mostFrequentDigit = digit;
-			maxFrequency = frequency;
-		}
-	}
+        if (frequency > 0 && frequency >= maxFrequency) {
+            mostFrequentDigit = digit;
+            maxFrequency = frequency;
+        }
+    }
 
-	return mostFrequentDigit;
+    return mostFrequentDigit;
 }
 
 int main()
 {
-	// Simple random test
-	ulonglong input1 = 87776908767;
-	_ASSERT(7 == getMostFrequentDigit(input1));
+    // Simple random test
+    ulonglong input1 = 87776908767;
+    _ASSERT(7 == getMostFrequentDigit(input1));
 
-	// Test that for equal (5) quantity of 7 and 8, the bigger one is picked
-	ulonglong input2 = 88778769087687;
-	_ASSERT(8 == getMostFrequentDigit(input2));
+    // Test that for equal (5) quantity of 7 and 8, the bigger one is picked
+    ulonglong input2 = 88778769087687;
+    _ASSERT(8 == getMostFrequentDigit(input2));
 
-	// Test that the biggest possible quantity of same digits (19 of 9-digit) can be handled
-	ulonglong input3 = 9999999999999999999UL;
-	_ASSERT(9 == getMostFrequentDigit(input3));
+    // Test that the biggest possible quantity of same digits (19 of 9-digit) can be handled
+    ulonglong input3 = 9999999999999999999UL;
+    _ASSERT(9 == getMostFrequentDigit(input3));
 
-
-	//cout << "The most frequent digit is " << getMostFrequentDigit(9999999999999999999UL) << endl;
+    cout << "All tests passed" << endl;
 }
